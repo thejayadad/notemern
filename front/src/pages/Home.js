865cd3fe4e31b9377f2e00cfd5ca33e3ce4443cf
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-02-19 09:44:57 -0500

    update

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -3,23 +3,20 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='wrapper'>
-    <ul>
+    <ul className='box'>
     <li>
      <a href='/' className='notes'>
             <h2>Note Title</h2>
-            <p>Note Content</p>
         </a>
      </li>
      <li>
      <a href='/' className='notes'>
             <h2>Note Title</h2>
-            <p>Note Content</p>
         </a>
      </li>
      <li>
      <a href='/' className='notes'>
             <h2>Note Title</h2>
-            <p>Note Content</p>
         </a>
      </li>
     </ul>
